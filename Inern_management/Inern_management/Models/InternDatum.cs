2f4dae4599c1480be7700d42f10f6cf8629e0fce
yaluoer <yaluoer1231@gmail.com>
2022-07-22 14:23:31 +0800

    套件與反向工程完成

---
+++ Inern_management/Inern_management/Models/InternDatum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Inern_management.Models
+{
+    public partial class InternDatum
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Sex { get; set; }
+        public string EMail { get; set; }
+    }
+}
