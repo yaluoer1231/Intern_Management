9dd27835b8879b3197a6550b8f3588c29d4358e0
yaluoer <yaluoer1231@gmail.com>
2022-08-15 11:37:32 +0800

    筆記區後端工程完成

--- Inern_management/Inern_management/Controllers/InternNoteController.cs
+++ Inern_management/Inern_management/Controllers/InternNoteController.cs
@@ -37,5 +37,71 @@ namespace Inern_management.Controllers
             return Ok(internNote);
         }
 
+        //POST
+        [HttpPost]
+        public async Task<ActionResult<InternNote>> PostIntern(InternNote internNote)
+        {
+            await _context.InternNotes.AddAsync(internNote);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetIternNote), new { id = internNote.Id }, internNote);
+        }
+
+
+        //PUT
+        [HttpPut("{id}")]
+        public async Task<ActionResult<InternNote>> PutIntern(int id, InternNote internNote)
+        {
+            if (id != internNote.Id)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(internNote).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!TodoInternExists(id))
+                {
+                    return NotFound(); //404錯誤
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+
+            return NoContent();
+        }
+
+        //DELETE
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<InternNote>> DeleteIternNote(int id)
+        {
+            var Note = _context.InternNotes.Where(t => t.Id == id).FirstOrDefault();
+
+            if (Note == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.InternNotes.Remove(Note);
+                await _context.SaveChangesAsync();
+                return Ok();
+
+            }
+        }
+
+        private bool TodoInternExists(int id)
+        {
+            return _context.InternNotes.Any(e => e.Id == id);
+        }
     }
 }
