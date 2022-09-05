9c364e6510cda4b5546b1d2657ca6a544d9feff9
yaluoer <yaluoer1231@gmail.com>
2022-09-05 17:29:00 +0800

    加入三元運算式的註解

--- Intern_management_system/src/app/input-directive.directive.ts
+++ Intern_management_system/src/app/input-directive.directive.ts
@@ -9,6 +9,12 @@ import { AbstractControl, FormControl, NG_VALIDATORS, PatternValidator, Validato
 })
 export class InputDirectiveDirective  implements Validator {
   @Input() name! : string;//抓到傳入物件的相對應的內容
+  
+  private readonly invalidResult = {
+    validatePhoneNumber :{
+      valid : false
+    }
+  };
 
   validate(c: FormControl) {
     if (!c.value) //如果沒有輸入文字，回傳空物件代表通過
@@ -18,7 +24,6 @@ export class InputDirectiveDirective  implements Validator {
     let cellphone = /^(09)(\d{2})[\s\-]?\d{3}[\s\-]?\d{3}$/;
     //^(09):開頭必須是09，括號可省略 \d：任意數字 {2}：限制兩個字 \s：空白 \-：減號 ?：可有可無 []：比對括號裡任一字元 $：結尾
 
-    
     if (Number(c.value.slice(0,2)) == 9){
       return cellphone.test(c.value) ? null :{
         validatePhoneNumber :{
